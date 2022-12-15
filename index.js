33894ed91f2f2d6cb5dcfa81c41d31377d9f3a38
abdihamid <ibrahimabdy58@gmail.com>
2022-12-15 19:58:49 +0300

    final

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-// variables
-let btn = document.querySelector('#new-quote');
-let quote = document.querySelector('.qoute');
-let character = document.querySelector('.character');
+const quoteEl = document.getElementById('quote');
+const authorEl = document.getElementById('author');
+const btnEl = document.getElementById('btn');
+
+// Get a random quote from the anime quote API
+const getQuote = async () => {
+  try {
+    const response = await fetch('https://animechan.vercel.app/api/quotes');
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const quote = data[randomIndex];
+
+    quoteEl.innerText = quote.quote;
+    authorEl.innerText = quote.author;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Initially get a quote
+getQuote();
+
+// When the button is clicked, get a new quote
+btnEl.addEventListener('click', getQuote);
